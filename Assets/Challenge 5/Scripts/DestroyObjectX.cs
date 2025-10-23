36c14d79a322d6fe969734097c2b29cd04730d71
TheMessiah111 <promizzdaniel@gmail.com>
2025-10-23 14:27:49 +0100

    Initial commit

---
+++ Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("What is this attached to: " + gameObject.name);
+        Destroy(gameObject, 2); // destroy particle after 2 seconds
+    }
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText; 
+    public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator SpawnTarget(){
+        while(isGameActive){
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0,targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+    public void UpdateScore(int ScoreToAdd){
+        score += ScoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+    public void GameOver(){
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        
+    }
+    public void RestartGame()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+    }
+}
